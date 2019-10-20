e8083a17c6d2c2936614a4caf3d220e5e77e15ce
Callum Whyte <hey@callumwhyte.com>
2019-10-20 01:59:59 +0100

    Making sitemap config accessible via the web.config

---
+++ src/Our.Umbraco.Sitemap/Startup/SitemapComposer.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+using Our.Umbraco.Sitemap.Configuration;
+using Umbraco.Core.Composing;
+
+namespace Our.Umbraco.Sitemap.Startup
+{
+    public class SitemapComposer : IComposer
+    {
+        private const string Prefix = "Umbraco.Sitemap";
+
+        public void Compose(Composition composition)
+        {
+            composition.RegisterUnique(factory => GetConfiguration());
+        }
+
+        private SitemapConfiguration GetConfiguration()
+        {
+            bool.TryParse(ConfigurationManager.AppSettings[Prefix + ".EnableSitemap"], out bool enableSitemap);
+
+            var configuration = new SitemapConfiguration
+            {
+                EnableSitemap = enableSitemap
+            };
+
+            return configuration;
+        }
+    }
+}
