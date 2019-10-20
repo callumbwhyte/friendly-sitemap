c78b2e2f3446c16d60a02212516f93bce20e7eef
Callum Whyte <hey@callumwhyte.com>
2019-10-20 02:01:48 +0100

    Generate XML document with URL and update date

--- src/Our.Umbraco.Sitemap/Controllers/SitemapController.cs
+++ src/Our.Umbraco.Sitemap/Controllers/SitemapController.cs
@@ -1,6 +1,11 @@
-﻿using System.Text;
+﻿using System.Linq;
+using System.Text;
 using System.Web.Mvc;
+using System.Xml.Linq;
 using Our.Umbraco.Sitemap.Configuration;
+using Our.Umbraco.Sitemap.Extensions;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Web;
 using Umbraco.Web.Mvc;
 
 namespace Our.Umbraco.Sitemap.Controllers
@@ -21,7 +26,38 @@ namespace Our.Umbraco.Sitemap.Controllers
                 return HttpNotFound();
             }
 
-            return Content("", "text/xml", Encoding.UTF8);
+            var sitemapXml = BuildSitemap();
+
+            return Content(sitemapXml, "text/xml", Encoding.UTF8);
+        }
+
+        private string BuildSitemap()
+        {
+            var startNode = UmbracoContext.PublishedRequest.PublishedContent;
+
+            var nodes = startNode
+                .Descendants()
+                .Where(x => x.IsVisible() == true && x.HasTemplate() == true);
+
+            XNamespace xmlns = "http://www.sitemaps.org/schemas/sitemap/0.9";
+            XElement root = new XElement(xmlns + "urlset");
+
+            foreach (var node in nodes)
+            {
+                var urlElement = new XElement("url", new[]
+                {
+                    new XElement("loc", node.Url(mode: UrlMode.Absolute)),
+                    new XElement("lastmod", node.UpdateDate.ToString("yyyy-MM-dd"))
+                });
+
+                root.Add(urlElement);
+            }
+
+            XDocument document = new XDocument(root);
+
+            var sitemapXml = document.ToString();
+
+            return sitemapXml;
         }
     }
 }
---
+++ src/Our.Umbraco.Sitemap/Extensions/PublishedContentExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Umbraco.Core.Models.PublishedContent;
+
+namespace Our.Umbraco.Sitemap.Extensions
+{
+    public static class PublishedContentExtensions
+    {
+        public static bool HasTemplate(this IPublishedContent content)
+        {
+            return content.TemplateId.HasValue == true;
+        }
+    }
+}
