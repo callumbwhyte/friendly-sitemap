ab35eb0fe661744c167b665ff923d83bce4b182a
Callum Whyte <hey@callumwhyte.com>
2019-10-20 02:03:34 +0100

    Routing sitemap.xml to controller through domain route handler

---
+++ src/Our.Umbraco.Sitemap/Startup/RouteComponent.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Routing;
+using Our.Umbraco.Sitemap.Routing;
+using Umbraco.Core.Composing;
+using Umbraco.Web;
+
+namespace Our.Umbraco.Sitemap.Startup
+{
+    internal class RouteComponet : IComponent
+    {
+        private readonly IUmbracoContextFactory _umbracoContextFactory;
+
+        public RouteComponet(IUmbracoContextFactory umbracoContextFactory)
+        {
+            _umbracoContextFactory = umbracoContextFactory;
+        }
+
+        public void Initialize()
+        {
+            RouteTable.Routes.MapUmbracoRoute(
+                "XML Sitemap",
+                "sitemap.xml",
+                new
+                {
+                    controller = "Sitemap",
+                    action = "RenderSitemap"
+                },
+                new RootNodeByDomainRouteHandler(_umbracoContextFactory)
+            );
+        }
+
+        public void Terminate()
+        {
+
+        }
+    }
+}
---
+++ src/Our.Umbraco.Sitemap/Startup/RouteComposer.cs
@@ -0,0 +1,13 @@
+﻿using Umbraco.Core;
+using Umbraco.Core.Composing;
+
+namespace Our.Umbraco.Sitemap.Startup
+{
+    public class RouteComposer : IUserComposer
+    {
+        public void Compose(Composition composition)
+        {
+            composition.Components().Insert<RouteComponet>();
+        }
+    }
+}
